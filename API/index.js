b8b025749040bea0d1a7b575e325bf2b6a707fc0
nindroz <mantha.hitesh@gmail.com>
2021-04-22 21:43:01 -0700

    airtable fetching works

--- API/index.js
+++ API/index.js
@@ -8,4 +8,25 @@ app.get("/", (req, res) => {
 	return res.send("Received a GET HTTP method");
 });
 
+app.get("/records", (req, res) => {
+	base("Table 1")
+		.select({
+			// Selecting the first 3 records in Grid view:
+			maxRecords: 3,
+			view: "Grid view",
+		})
+		.eachPage(function page(records, fetchNextPage) {
+			// This function (`page`) will get called for each page of records.
+
+			records.forEach(function (record) {
+				console.log("Retrieved", record.get("Content"));
+			});
+
+			// To fetch the next page of records, call `fetchNextPage`.
+			// If there are more records, `page` will get called again.
+			// If there are no more records, `done` will get called.
+			fetchNextPage();
+		});
+});
+
 app.listen(3000, () => console.log(`Example app listening on port 3000!`));
