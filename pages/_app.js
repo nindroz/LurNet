0bdef1410857f14c431af379c3d45e633a52572b
nindroz <mantha.hitesh@gmail.com>
2021-05-04 21:26:43 -0700

    added chakra

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider, theme, CSSReset } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<ChakraProvider theme={theme}>
+			<CSSReset />
+			<Component {...pageProps} />
+		</ChakraProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
