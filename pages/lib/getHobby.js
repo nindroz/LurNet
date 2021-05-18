f81094839c829bca696252e7ef8969112a2c1cb7
nindroz <mantha.hitesh@gmail.com>
2021-05-17 20:30:35 -0700

    airtable fetching finished )

--- pages/lib/getHobby.js
+++ pages/lib/getHobby.js
@@ -6,12 +6,26 @@ const base = new Airtable({
 
 const table = base("main");
 
+// maps over the records, calling minifyRecord, giving us required data
+const getMinifiedRecords = records => {
+  return records.map(record => minifyRecord(record));
+};
+
+// gets the data we want and puts it into variables
+const minifyRecord = record => {
+  return {
+    id: record.id,
+    fields: record.fields,
+  };
+};
+
+
 export default async function getHobby() {
   const records = await table.select({}).all();
+  const minifiedRecords = await getMinifiedRecords(records);
+  console.log(minifiedRecords);
 
-  console.log(records);
-
-  return records;
+  return minifiedRecords;
 }
 
 
