289aa459be82d876e75ebaaf0eca77f32d53a719
nindroz <mantha.hitesh@gmail.com>
2021-05-19 21:53:47 -0700

    404 added

--- pages/hobby/[hname].js
+++ pages/hobby/[hname].js
@@ -1,15 +1,20 @@
 import React from "react";
+import Error from 'next/error'
 import getHobby from "../lib/getHobby"
 
 const Hobby = ({data}) => {
-	return data[0].fields.Content;
+	if(data.length==0){
+		return <Error statusCode={404} />
+	} else {
+		return data[0].fields.Content;
+	}
+		
 }
 
 export async function getServerSideProps(context) {
 	const data = await getHobby(context.query.hname);
+	//move 404 handling here
 	return { props: { data } }
-  }
+}
 
 export default Hobby;	
-
-// IMPORTANT!!! Figure out async issues with calling getHobby and returning returned value
